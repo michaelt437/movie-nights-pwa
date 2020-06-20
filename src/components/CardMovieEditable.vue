05fd0506fbf00d52b9f4bb2763414014ca1b9132
michael tran <michaelt437@gmail.com>
2020-06-20 02:00:42 -0400

    scope exclude styles to only the editable cards

--- src/components/CardMovieEditable.vue
+++ src/components/CardMovieEditable.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="movie-card rounded-lg text-gray-200 px-5 py-3 mb-4 flex" :class="exclude ? 'bg-gray-900 movie-card--exclude' : 'bg-gray-800'">
+  <div class="movie-card rounded-lg text-gray-200 px-5 py-3 mb-4 flex" :class="excludeMovie ? 'bg-gray-900 movie-card--exclude' : 'bg-gray-800'">
     <div class="flex-grow">
       <div class="movie-card__title text-2xl">
         {{ movie.title }}
@@ -10,9 +10,9 @@
       </div>
     </div>
     <div class="flex flex-col justify-between items-center py-1">
-      <label for="exclude">
-        <input type="checkbox" name="exclude" id="exclude" v-model="exclude" hidden>
-        <i class="text-2xl text-blue-400 cursor-pointer" :class="exclude ? 'far fa-circle' : 'fas fa-check-circle'"></i>
+      <label :for="`exclude-${titleCamelCase}`">
+        <input type="checkbox" name="excludeMovie" :id="`exclude-${titleCamelCase}`" v-model="excludeMovie" hidden>
+        <i class="text-2xl text-blue-400 cursor-pointer" :class="excludeMovie ? 'far fa-circle' : 'fas fa-check-circle'"></i>
       </label>
       <div class="movie-card__delete text-red-400"><i class="fas fa-trash"></i></div>
     </div>
@@ -27,6 +27,21 @@ import IMovie from "@/interface/IMovie";
 export default class CardMovie extends Vue {
   @Prop(Object) readonly movie!: IMovie;
 
-  private exclude = false;
+  private excludeMovie = false;
+
+  get titleCamelCase () {
+    return this.movie.title.split(" ").join("");
+  }
 }
 </script>
+<style lang="scss" scoped>
+  .movie-card, .movie-card > * {
+    transition: all .2s ease-out;
+  }
+
+  .movie-card--exclude {
+    .movie-card__title, .movie-card__service, .movie-card__duration {
+      opacity: .5;
+    }
+  }
+</style>
