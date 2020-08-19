447b865be4fc49dc7155739a77ce6d92bb1545fe
michaelt437 <michaelt437@gmail.com>
2020-08-19 18:50:46 -0400

    testing favicon fix

--- postDeploy.js
+++ postDeploy.js
@@ -5,6 +5,6 @@ const msg = JSON.stringify({
   text: ":niccage: Update released!\n>1. movienights.netlify.app\n>2. Visit on your phone and add the app to home page"
 });
 
-xhr.open("POST", process.env.DEPLOY_HOOK);
-xhr.send(msg);
-console.log("hooked!");
+// xhr.open("POST", process.env.DEPLOY_HOOK);
+// xhr.send(msg);
+// console.log("hooked!");
