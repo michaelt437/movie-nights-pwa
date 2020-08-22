7d374c7e7b81d1074cc83f1bcc29cf73d9416f7d
michaelt437 <michaelt437@gmail.com>
2020-08-22 17:16:09 -0400

    feature--8: postDeploy update

--- postDeploy.js
+++ postDeploy.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const xhr = new XMLHttpRequest();
 const msg = JSON.stringify({
-  text: ":niccage: Update released!\n>1. roll confirm should post to slack"
+  text: ":niccage: Update released!\n>1. fix width of cards on web\n2. turn off autocomplete in forms"
 });
 
 xhr.open("POST", process.env.DEPLOY_HOOK);
