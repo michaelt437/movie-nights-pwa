8bed329fbbc70627679ae3ac4976eeba95f80dc8
Michael Tran <michaelt437@gmail.com>
2021-07-19 00:32:06 -0400

    add clear search

--- src/views/ListPage.vue
+++ src/views/ListPage.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="list-page" class="relative flex-grow md:flex-grow-0">
-    <div class="flex items-center mb-12 border border-gray-700 rounded-md px-3 py-1">
+    <div
+      class="flex items-center mb-12 border border-gray-700 rounded-md px-3 py-1"
+    >
       <i class="fas fa-search text-gray-500 absolute"></i>
-      <div class="input search w-full">
+      <div class="input search w-full relative">
         <input
           type="text"
           id="search-input"
@@ -11,24 +13,35 @@
           autocomplete="off"
           class="pl-6 w-full outline-none"
         />
+        <span v-show="searchInput !== ''" id="search-clear" @click="clear()">
+          <i class="fas fa-times text-gray-500"></i>
+        </span>
       </div>
     </div>
-    <div class="btn-group items-center justify-between mb-4 relative text-gray-200">
+    <div
+      class="btn-group items-center justify-between mb-4 relative text-gray-200"
+    >
       <div class="btn btn-pink-600 pointer-events-none">
         <span v-show="activeFilters">Showing</span> {{ filteredMovies.length }}
       </div>
       <button
         class="btn border text-md"
         :class="activeFilters ? 'btn-green-400' : 'btn-gray-500 outline'"
-        @click="invokeDrawer">
-        <span v-show="activeFilters">{{ activeFilters }}</span> Filter<span v-show="activeFilters > 1">s</span> <i class="fas fa-sliders-h ml-2"></i>
+        @click="invokeDrawer"
+      >
+        <span v-show="activeFilters">{{ activeFilters }}</span> Filter<span
+          v-show="activeFilters > 1"
+          >s</span
+        >
+        <i class="fas fa-sliders-h ml-2"></i>
       </button>
     </div>
     <template v-for="movie in filteredMovies">
       <card-movie-editable
-      v-on="$listeners"
-      :key="movie.title"
-      :movie="movie" />
+        v-on="$listeners"
+        :key="movie.title"
+        :movie="movie"
+      />
     </template>
     <div class="pt-4 py-16 text-gray-600 text-center">Your beginning...</div>
   </div>
@@ -46,18 +59,25 @@ import IMovie from "@/interface/IMovie";
 export default class ListPage extends Vue {
   public searchInput = "";
 
-  get moviesToWatch (): Array<IMovie> {
+  get moviesToWatch(): Array<IMovie> {
     return this.$store.getters.getMoviesToWatch;
   }
 
-  get filteredMovies (): Array<IMovie> {
-    return this.$store.getters.getMoviesToWatch.filter(movie => {
-      return movie.title.toLowerCase().includes(this.searchInput.toLowerCase()) ||
-        movie.service.title.toLowerCase().includes(this.searchInput.toLowerCase());
-    })
+  get filteredMovies(): Array<IMovie> {
+    return this.$store.getters.getMoviesToWatch
+      .filter(movie => {
+        return (
+          movie.title.toLowerCase().includes(this.searchInput.toLowerCase()) ||
+          movie.service.title
+            .toLowerCase()
+            .includes(this.searchInput.toLowerCase())
+        );
+      })
       .filter(movie => {
         if (this.$store.getters.getServiceFilters.length) {
-          return this.$store.getters.getServiceFilters.includes(movie.service.value);
+          return this.$store.getters.getServiceFilters.includes(
+            movie.service.value
+          );
         } else {
           return true;
         }
@@ -70,7 +90,10 @@ export default class ListPage extends Vue {
             }
           }
           if (this.$store.getters.getDurationFilters.includes("long")) {
-            if (Number(movie.duration) >= 107 && Number(movie.duration) <= 134) {
+            if (
+              Number(movie.duration) >= 107 &&
+              Number(movie.duration) <= 134
+            ) {
               return movie;
             }
           }
@@ -97,7 +120,9 @@ export default class ListPage extends Vue {
           case "alpha":
             if (movie1.title.toLowerCase() > movie2.title.toLowerCase()) {
               return 1;
-            } else if (movie1.title.toLowerCase() < movie2.title.toLowerCase()) {
+            } else if (
+              movie1.title.toLowerCase() < movie2.title.toLowerCase()
+            ) {
               return -1;
             } else {
               return 0;
@@ -119,9 +144,15 @@ export default class ListPage extends Vue {
               return 0;
             }
           case "service_abc":
-            if (movie1.service.value.toLowerCase() > movie2.service.value.toLowerCase()) {
+            if (
+              movie1.service.value.toLowerCase() >
+              movie2.service.value.toLowerCase()
+            ) {
               return 1;
-            } else if (movie1.service.value.toLowerCase() < movie2.service.value.toLowerCase()) {
+            } else if (
+              movie1.service.value.toLowerCase() <
+              movie2.service.value.toLowerCase()
+            ) {
               return -1;
             } else {
               return 0;
@@ -154,7 +185,7 @@ export default class ListPage extends Vue {
       });
   }
 
-  get activeFilters (): number {
+  get activeFilters(): number {
     let count = 0;
     if (this.$store.state.orderFilter !== "") count++;
     if (this.$store.state.excludeFilter !== "") count++;
@@ -164,11 +195,15 @@ export default class ListPage extends Vue {
     return count;
   }
 
-  invokeDrawer (): void {
+  invokeDrawer(): void {
     this.$emit("drawer", "DrawerFilter");
   }
 
-  beforeDestroy (): void {
+  clearSearch(): void {
+    this.searchInput = "";
+  }
+
+  beforeDestroy(): void {
     this.$store.commit("resetListPageFilters");
   }
 }
