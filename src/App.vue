ab3a45b035658c377096871c5831bbf4fb3e5c0c
Michael Tran <michaelt437@gmail.com>
2021-10-10 12:38:20 -0400

    app: remove unused import Watch

--- src/App.vue
+++ src/App.vue
@@ -54,7 +54,7 @@
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue, Watch } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 import AppHeader from "@/components/AppHeader.vue";
 import AppParalaxBackground from "@/components/AppParalaxBackground.vue";
 import AppTitle from "@/components/AppTitle.vue";
