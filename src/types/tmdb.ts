f0ab958eb816722d3cbc522fbeef1b7daa461573
Michael Tran <michaelt437@gmail.com>
2021-11-06 12:05:39 -0400

    tmdb: add movie details and genre types

--- src/types/tmdb.ts
+++ src/types/tmdb.ts
@@ -43,7 +43,42 @@ type TMDBStreamProvider = {
   provider_id: number;
 };
 
+type TMDBGenre = {
+  id: number;
+  name: string;
+};
+
+type TMDBMovie = {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: object;
+  budget: number;
+  genres: TMDBGenre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: any[];
+  production_countries: any[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: any[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
 export {
+  TMDBGenre,
+  TMDBMovie,
   TMBDMovieSearch,
   TMDBSearchDTO,
   TMDBStreamProvider,
