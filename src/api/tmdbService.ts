848cff0422ca3f5ed593aaead79cdb59a51d12f6
Michael Tran <michaelt437@gmail.com>
2021-11-07 10:45:15 -0500

    tmdb config: add type and request to service

--- src/api/tmdbService.ts
+++ src/api/tmdbService.ts
@@ -5,15 +5,16 @@ import {
   TMBDMovieSearch,
   TMDBSearchDTO,
   TMDBStreamProvider,
-  TMDBWatchProviderDTO
+  TMDBWatchProviderDTO,
+  TMDBConfig
 } from "@/types/tmdb";
 
 const uriRoot = "https://api.themoviedb.org/3/";
 
 class TMDBService
-implements
+  implements
     IMovieDatabaseService<TMBDMovieSearch, TMDBStreamProvider, TMDBMovie> {
-  async getWatchProviders (movieId: number): Promise<TMDBStreamProvider[]> {
+  async getWatchProviders(movieId: number): Promise<TMDBStreamProvider[]> {
     const apiResponse = await axios.get<TMDBWatchProviderDTO>(
       `${uriRoot}movie/${movieId}/watch/providers?api_key=${process.env.VUE_APP_TMDBKEY}`
     );
@@ -21,7 +22,7 @@ implements
     return results ?? [];
   }
 
-  async searchMovie (searchText: string): Promise<TMBDMovieSearch[]> {
+  async searchMovie(searchText: string): Promise<TMBDMovieSearch[]> {
     const apiResponse = await axios.get<TMDBSearchDTO>(
       `${uriRoot}search/movie?api_key=${
         process.env.VUE_APP_TMDBKEY
@@ -33,9 +34,17 @@ implements
     return results ?? [];
   }
 
-  async getMovieDetails (movieId: number): Promise<TMDBMovie> {
+  async getMovieDetails(movieId: number): Promise<TMDBMovie> {
     const apiResponse = await axios.get<TMDBMovie>(
       `${uriRoot}movie/${movieId}?api_key=${process.env.VUE_APP_TMDBKEY}`
+    );
+    const result = apiResponse.data;
+    return result ?? {};
+  }
+
+  async getConfiguration(): Promise<TMDBConfig> {
+    const apiResponse = await axios.get<TMDBConfig>(
+      `${uriRoot}configuration?api_key=${process.env.VUE_APP_TMDBKEY}`
     );
     const result = apiResponse.data;
     return result ?? {};
--- src/types/tmdb.ts
+++ src/types/tmdb.ts
@@ -76,7 +76,20 @@ type TMDBMovie = {
   vote_count: number;
 };
 
+type TMDBConfig = {
+  images: {
+    base_url: string;
+    secure_base_url: string;
+    backdrop_sizes: string[];
+    logo_sizes: string[];
+    poster_sizes: string[];
+    profile_sizes: string[];
+    still_sizes: string[];
+  };
+  change_keys: string[];
+};
 export {
+  TMDBConfig,
   TMDBGenre,
   TMDBMovie,
   TMBDMovieSearch,
