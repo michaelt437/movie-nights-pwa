f97fd95e8508b3f754d3468353c3b0505cf66d7d
Michael Tran <michaelt437@gmail.com>
2021-11-07 12:49:29 -0500

    store: fetch and set config

--- src/types/interface/IMovieDatabaseService.ts
+++ src/types/interface/IMovieDatabaseService.ts
@@ -2,6 +2,7 @@ interface IMovieDatabaseService<M, S, T> {
   getWatchProviders(movieId: number): Promise<S[]>;
   searchMovie(searchText: string): Promise<M[]>;
   getMovieDetails(movieId: number): Promise<T>;
+  getConfiguration(): Promise<any>;
 }
 
 export default IMovieDatabaseService;
