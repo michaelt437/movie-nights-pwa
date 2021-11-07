41ff57b66f00f0c4b0425d5abb192d7671cc5bb8
Michael Tran <michaelt437@gmail.com>
2021-11-06 23:03:42 -0500

    IMovie: simplify imovie

--- src/types/interface/IMovie.ts
+++ src/types/interface/IMovie.ts
@@ -1,26 +1,11 @@
-import IService from "./IService";
-import { TMBDMovieSearch, TMDBStreamProvider } from "@/types/tmdb";
+import { TMDBMovie, TMDBStreamProvider } from "@/types/tmdb";
 
-type IMovieBase = {
-  title?: string;
-  service?: IService;
-  duration?: number | string;
-  hasWatched?: boolean;
-  watchDate?: number;
-  exclude?: boolean;
+type IMovie = TMDBMovie & {
+  hasWatched: boolean;
+  addedDate: number;
+  exclude: boolean;
   documentId?: string;
   user?: string;
-  genres?: Array<object>;
 };
-
-type IMovie = IMovieBase &
-  TMBDMovieSearch & {
-    hasWatched: boolean;
-    addedDate: number;
-    exclude: boolean;
-    documentId?: string;
-    providers: TMDBStreamProvider[];
-    user?: string;
-  };
 
 export default IMovie;
