4ae852df17f6220a7880e2c6ebbf46425ed89c5f
Michael Tran <michaelt437@gmail.com>
2021-11-07 23:32:31 -0500

    IMovie: adjustment to type

--- src/types/interface/IMovie.ts
+++ src/types/interface/IMovie.ts
@@ -4,8 +4,13 @@ type IMovie = TMDBMovie & {
   hasWatched: boolean;
   addedDate: number;
   exclude: boolean;
+  watchDate: number;
+  providers: TMDBStreamProvider[];
   documentId?: string;
   user?: string;
+  // TODO remove old members
+  service?: any;
+  duration?: number;
 };
 
 export default IMovie;
