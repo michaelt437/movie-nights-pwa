009d86476164a176666f4e55ea3e562841df5830
Michael Tran <michaelt437@gmail.com>
2021-11-14 11:13:50 -0500

    cardEditRollTonights: update isRewatch with tmdb movie id

--- src/components/CardMovieEditable.vue
+++ src/components/CardMovieEditable.vue
@@ -109,10 +109,15 @@ export default class CardMovieEditable extends Vue {
   get isRewatch (): boolean {
     return Boolean(
       this.$store.getters.getMoviesWatched.find((paramMovie: IMovie) => {
-        return (
-          paramMovie.title.toLowerCase() === this.movie.title.toLowerCase() &&
-          paramMovie.hasWatched === true
-        );
+        if (paramMovie.service) {
+          return (
+            paramMovie.title.toLowerCase() === this.movie.title.toLowerCase() &&
+            paramMovie.hasWatched
+          );
+        }
+        if (paramMovie.id) {
+          return paramMovie.id === this.movie.id && paramMovie.hasWatched;
+        }
       })
     );
   }
