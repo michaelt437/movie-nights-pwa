eade7e81645cf3caa1e7f46a10d96501d715a533
Michael Tran <michaelt437@gmail.com>
2021-11-16 00:33:38 -0500

    popupedit: add select for current providers if providers exist

--- src/components/PopupEditMovie.vue
+++ src/components/PopupEditMovie.vue
@@ -1,24 +1,34 @@
 <template>
-  <div
-    class="flex flex-col rounded-lg bg-indigo-600 text-gray-200 px-5 mb-4 h-full"
-  >
+  <div class="flex flex-col rounded-lg bg-gray-200 text-gray-600 px-5 mb-4">
     <p class="text-2xl text-center py-5">Edit Details</p>
     <div class="popup-content overflow-y-auto">
       <!-- home video option? -->
+      <template v-if="movie.providers.length">
+        <label for="providers">Edit Primary Provider</label>
+        <select id="providers" class="text-sm" v-model="selectedProvider">
+          <option
+            v-for="provider in movie.providers"
+            :key="provider.provider_id"
+            :value="provider"
+          >
+            {{ provider.provider_name }}
+          </option>
+        </select>
+      </template>
     </div>
-    <div class="btn-group flex py-3">
+    <div class="btn-group flex py-5">
       <span class="ml-auto"></span>
       <button
         class="btn btn-gray-400 outline"
-        style="flex-basis: 30%;"
+        style="flex-basis: 30%"
         @click="closePopup"
       >
         <i class="fas fa-times mr-1"></i> Cancel
       </button>
       <button
         :class="{ disabled: disableButton }"
-        class="btn btn-green-400"
-        style="flex-basis: 30%;"
+        class="btn btn-green-400 text-white"
+        style="flex-basis: 30%"
         @click="submitEdits"
       >
         <i class="fas fa-check mr-1"></i> Save
@@ -29,25 +39,17 @@
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
 import IMovie from "@/types/interface/IMovie";
-import IGenre from "@/types/interface/IGenre";
 import { db } from "@/db";
 import { omit, isEqual } from "lodash";
+import { TMDBStreamProvider } from "@/types/tmdb";
 
 @Component
 export default class PopupEditMovie extends Vue {
   @Prop(Object) readonly movie!: IMovie;
   @Prop() readonly action?: Function;
 
-  movieToEdit: any = {
-    documentId: "",
-    title: "",
-    service: {
-      title: "",
-      value: ""
-    },
-    duration: 0,
-    genres: []
-  };
+  movieToEdit: IMovie = {} as IMovie;
+  selectedProvider: TMDBStreamProvider = {} as TMDBStreamProvider;
 
   get disableButton (): boolean {
     return isEqual(this.movie, this.movieToEdit);
@@ -55,14 +57,6 @@ export default class PopupEditMovie extends Vue {
 
   get movieToEditOmitId (): IMovie {
     return omit(this.movieToEdit, "documentId");
-  }
-
-  hasGenre (genre: string): object {
-    return {
-      active: this.movieToEdit.genres.find(
-        (genreObj: IGenre) => genreObj.value === genre
-      )
-    };
   }
 
   closePopup (): void {
@@ -81,6 +75,7 @@ export default class PopupEditMovie extends Vue {
 
   mounted () {
     this.movieToEdit = JSON.parse(JSON.stringify(this.movie));
+    if (this.movieToEdit.providers.length) { this.selectedProvider = this.movieToEdit.providers[0]; }
   }
 }
 </script>
